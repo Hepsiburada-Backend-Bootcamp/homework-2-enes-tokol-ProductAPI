2d25ad620d0517c12aae88d0d1d34019886890d3
Enes <enestokol@gmail.com>
2021-10-02 09:32:16 +0300

    Add ProductSeed

---
+++ Domain/Seeds/ProductSeed.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Domain.Seeds
+{
+    public class ProductSeed:IEntityTypeConfiguration<Product>
+    {
+        public ProductSeed()
+        {
+        }
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(
+                new Product { Id = 1,Name = "Sony Xperia L1"},
+                new Product { Id = 2,Name = "Huawei Mate 9 "},
+                new Product { Id = 3,Name = "Iphone 13"},
+                new Product { Id = 4,Name = "Redmi Note 8"});
+        }
+    }
+}
