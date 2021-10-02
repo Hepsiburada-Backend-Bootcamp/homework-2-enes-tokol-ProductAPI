5b49e2b21a9e93b291fab0565dff48e47dc693ea
Enes <enestokol@gmail.com>
2021-10-02 09:32:04 +0300

    Add Configurations

---
+++ Domain/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Domain.Configurations
+{
+    public class CategoryConfiguration:IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("categories");
+            
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.Id).UseMySqlIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .HasColumnType("nvarchar")
+                .HasMaxLength(255)
+                .IsRequired();
+        }
+    }
+}
---
+++ Domain/Configurations/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Domain.Configurations
+{
+    public class ProductConfiguration:IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("products");
+            
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.Id).UseMySqlIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .HasColumnType("nvarchar")
+                .HasMaxLength(255)
+                .IsRequired();
+            
+            builder.HasOne<Category>(x => x.Category)
+                .WithMany(s => s.Products)
+                .HasForeignKey(x => x.CategoryId)
+                .IsRequired();
+        }
+    }
+}
