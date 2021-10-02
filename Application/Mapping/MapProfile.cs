26c6aebdb18e5a9940d005f0c9f91f361b8105ac
Enes <enestokol@gmail.com>
2021-10-02 09:34:25 +0300

    Add MapProfile for AutoMapper

---
+++ Application/Mapping/MapProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.Dtos.Products;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            // Product DTO Profiles
+            CreateMap<ProductCreateDto, Product>().ReverseMap();
+
+        }
+    }
+}
