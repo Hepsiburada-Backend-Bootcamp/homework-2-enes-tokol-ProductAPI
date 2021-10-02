077fc56bb4430923a6ba81a4176bad08c490e63e
Enes <enestokol@gmail.com>
2021-10-02 09:34:11 +0300

    Add centralized response

---
+++ Application/Responses/Response.cs
@@ -0,0 +1,25 @@
+﻿namespace Application.Responses
+{
+    public class Response<TEntity>
+    {
+        public TEntity Data { get; set; }
+
+        public bool Success { get; set; }
+
+        public string ErrorMessage { get; set; }
+        
+
+        public Response(TEntity data)
+        {
+            Success = true;
+            Data = data;
+        }
+        
+        public Response(string errorMessage)
+        {
+            Success = false;
+            ErrorMessage = errorMessage;
+        }
+        
+    }
+}
