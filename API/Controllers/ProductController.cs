806fac5b4f0d7bf927a4377ce84c4b08e7bf624f
Enes <enestokol@gmail.com>
2021-10-02 09:35:43 +0300

    Add Controllers

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Product Controller
+    /// </summary>
+    [Route("api/Products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        
+
+    }
+}
