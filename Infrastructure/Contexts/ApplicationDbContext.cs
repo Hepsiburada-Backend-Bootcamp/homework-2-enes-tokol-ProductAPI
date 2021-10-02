e723c73b84568caa8c92a66ebde487ffcfd8bb4e
Enes <enestokol@gmail.com>
2021-10-02 09:33:25 +0300

    Add dbContext

---
+++ Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Configurations;
+using Domain.Entities;
+using Domain.Seeds;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductSeed());
+        }
+    }
+}
