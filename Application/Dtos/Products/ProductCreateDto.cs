58f532172413d1fceec98b10b7aa30b2cab9734d
Enes <enestokol@gmail.com>
2021-10-02 09:34:35 +0300

    Add Product Create DTO

---
+++ Application/Dtos/Products/ProductCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Dtos.Products
+{
+    /// <summary>
+    /// Product Create DTO
+    /// </summary>
+    public class ProductCreateDto
+    {
+        
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
